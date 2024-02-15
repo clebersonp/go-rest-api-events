d6d17d50e537cd3517ddc9abb2c223ee0b3fb602
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 13:53:06 -0300

    Created go file to route events resources in a separeted file

---
+++ routes/events_routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"example.com/rest-api-events/http_handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterEventsRoutes(server *gin.Engine) {
+	// call via curl: curl -s -X GET "http://localhost:8080/events" | json_pp
+	server.GET("/events", http_handlers.GetEvents)
+	server.GET("/events/:id", http_handlers.GetEventByID)
+	server.POST("/events", http_handlers.CreateEvent)
+	server.PUT("/events/:id", http_handlers.UpdateEvent)
+	server.DELETE("/events/:id", http_handlers.DeleteEvent)
+}
