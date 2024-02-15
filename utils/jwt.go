830d333fc9d231e63b466db6264f95048fc0acda
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 17:18:59 -0300

    Fixed signing key type to generate token

--- utils/jwt.go
+++ utils/jwt.go
@@ -9,11 +9,13 @@ import (
 
 const secretKey = "superSecret"
 
-func GenerateToken(email string, userId int64) (string, error) {
+func GenerateToken(userId int64, email string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email":   email,
 		"user_id": userId,
+		"email":   email,
 		"exp":     time.Now().Add(time.Hour * 2).Unix(),
 	})
-	return token.SignedString(secretKey)
+	// the correct type by signing method chosen:
+	// https://golang-jwt.github.io/jwt/usage/signing_methods/#signing-methods-and-key-types
+	return token.SignedString([]byte(secretKey))
 }
