d7bce45a322d3620368b517b7c94424ffb62efbd
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 13:57:05 -0300

    Refactored register routes file and call routes for users and events separately

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,11 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-// RegisterRoutes - Routes all available endpoint and call their handlers from other file
+// RegisterRoutes - Routes all available endpoint and call their http_handlers from other file
 func RegisterRoutes(server *gin.Engine) {
-	// call via curl: curl -s -X GET "http://localhost:8080/events" | json_pp
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEventByID)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
-
+	RegisterEventsRoutes(server)
+	RegisterUsersRoutes(server)
 }
