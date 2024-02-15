684af13010db005f87fb5cb53e845746562d7feb
Cleberson <pauluci.cleberson@gmail.com>
2024-02-14 21:06:05 -0300

    Refactor event and function GetAllEventes from models, added gin binding struct tags to validate missing fields

--- models/event.go
+++ models/event.go
@@ -2,12 +2,14 @@ package models
 
 import "time"
 
+// Event - struct tags for json deserialized string into structs and
+// binding feature from gin package to required missing fields
 type Event struct {
 	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Location    string    `json:"location"`
-	DateTime    time.Time `json:"date_time"`
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
+	DateTime    time.Time `json:"date_time" binding:"required"`
 	UserID      int       `json:"user_id"`
 }
 
@@ -16,4 +18,11 @@ var events []Event
 func (e *Event) Save() {
 	// later: add it to a database
 	events = append(events, *e)
+}
+
+func GetAllEvents() []Event {
+	if events == nil {
+		return []Event{}
+	}
+	return events
 }
