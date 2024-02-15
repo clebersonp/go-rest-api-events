aa840aba99af604c7d120fff4145f2b8ebdd47a2
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 16:43:45 -0300

    Created a new handler func to login user

--- http_handlers/users_handler.go
+++ http_handlers/users_handler.go
@@ -23,3 +23,25 @@ func Signup(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "User created!"})
 }
+
+func Login(context *gin.Context) {
+	user := models.User{}
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		fmt.Println(err)
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Failed to parse data", "error": err.Error()})
+		return
+	}
+
+	isValid, err := user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to login the user!", "error": err.Error()})
+		return
+	}
+
+	if !isValid {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "User unauthorized!", "error": err})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "User logged!"})
+}
