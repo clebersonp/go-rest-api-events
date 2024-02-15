bb12b95930c97392b3f9ae45ea9e21b8ee6baf94
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 12:11:52 -0300

    Created route for delete event by id

--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,6 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events/:id", getEventByID)
 	server.POST("/events", createEvent)
 	server.PUT("/events/:id", updateEvent)
+	server.DELETE("/events/:id", deleteEvent)
 
 }
