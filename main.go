52b9bf4964eeaa5de183456cb21aa8cd7fa971a9
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 10:38:22 -0300

    Refactored main.go file to call routes and then keep it cleaner

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"example.com/rest-api-events/db"
-	"example.com/rest-api-events/models"
+	"example.com/rest-api-events/routes"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strconv"
 )
 
 // to install the third-party Gin package to project: go get -u github.com/gin-gonic/gin
@@ -19,66 +17,12 @@ func main() {
 
 	server := gin.Default()
 
-	// call via curl: curl -s -X GET "http://localhost:8080/events" | json_pp
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEventByID)
-	server.POST("/events", createEvent)
+	// Register all endpoints and theirs handler function
+	routes.RegisterRoutes(server)
 
 	err := server.Run(":8080") // by default will be localhost:
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-}
-
-// getEvents - will be used as named function by handler
-func getEvents(context *gin.Context) {
-	// with gin all we need to do we do with context
-	events, err := models.GetAllEvents()
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not retrieve data", "error": err})
-		return
-	}
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	event := models.Event{}
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		fmt.Println(err)
-		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("Failed to parse data: %v\n", err)})
-		return
-	}
-
-	event.UserID = 1
-	err = event.Save()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event!", "error": err})
-		return
-	}
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
-}
-
-func getEventByID(context *gin.Context) {
-	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "ID can't converted to integer!", "error": err.Error()})
-		return
-	}
-
-	event, err := models.GetEventByID(id)
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get event by ID!", "error": err})
-		return
-	}
-
-	if event == nil {
-		context.JSON(http.StatusNotFound, gin.H{"message": "Event Not Found!", "error": err})
-		return
-	}
-
-	context.JSON(http.StatusOK, event)
 }
