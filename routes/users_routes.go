85d002faa299a9daeb08ae21affb3f9f46e130a9
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 13:53:22 -0300

    Created go file to route users resources in a separeted file

---
+++ routes/users_routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"example.com/rest-api-events/http_handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUsersRoutes(server *gin.Engine) {
+	server.POST("/users", http_handlers.Signup)
+}
