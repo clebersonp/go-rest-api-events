7ddce71f2961370c1b128842927fe54436b2ceec
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 22:38:36 -0300

    Added manually the auth middleware to POST events to create an event

--- routes/events_routes.go
+++ routes/events_routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"example.com/rest-api-events/http_handlers"
+	"example.com/rest-api-events/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,7 +10,7 @@ func RegisterEventsRoutes(server *gin.Engine) {
 	// call via curl: curl -s -X GET "http://localhost:8080/events" | json_pp
 	server.GET("/events", http_handlers.GetEvents)
 	server.GET("/events/:id", http_handlers.GetEventByID)
-	server.POST("/events", http_handlers.CreateEvent)
+	server.POST("/events", middlewares.Authenticate, http_handlers.CreateEvent)
 	server.PUT("/events/:id", http_handlers.UpdateEvent)
 	server.DELETE("/events/:id", http_handlers.DeleteEvent)
 }
