a4714f3a60ab72bd7b2c9a9d472956857fd4869e
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 22:54:42 -0300

    Created gin group router to register endpoints that need to be authenticated by middleware

--- routes/events_routes.go
+++ routes/events_routes.go
@@ -8,9 +8,14 @@ import (
 
 func RegisterEventsRoutes(server *gin.Engine) {
 	// call via curl: curl -s -X GET "http://localhost:8080/events" | json_pp
+	// example of endpoints without authentication
 	server.GET("/events", http_handlers.GetEvents)
 	server.GET("/events/:id", http_handlers.GetEventByID)
-	server.POST("/events", middlewares.Authenticate, http_handlers.CreateEvent)
-	server.PUT("/events/:id", http_handlers.UpdateEvent)
-	server.DELETE("/events/:id", http_handlers.DeleteEvent)
+
+	// create a group to register the middlewares only once to authenticate user
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+	authenticated.POST("/events", middlewares.Authenticate, http_handlers.CreateEvent)
+	authenticated.PUT("/events/:id", http_handlers.UpdateEvent)
+	authenticated.DELETE("/events/:id", http_handlers.DeleteEvent)
 }
