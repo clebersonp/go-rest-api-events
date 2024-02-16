d52638c1699b0f58c1373a752edaa5e8932bfee2
Cleberson <pauluci.cleberson@gmail.com>
2024-02-15 22:37:56 -0300

    Created an auth middleware handler

---
+++ middlewares/auth.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"example.com/rest-api-events/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func Authenticate(context *gin.Context) {
+	authorization := context.Request.Header.Get("Authorization")
+	token, found := strings.CutPrefix(authorization, "Bearer ")
+
+	if authorization == "" || !found {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	// set to context the user_id as key-value
+	context.Set("user_id", strconv.FormatInt(userId, 10))
+	context.Next()
+}
